483de05fbfd4b4d7b5f267706ec787eea1230da4
Gunhoon Lee <gunhoon@gmail.com>
2023-05-20 12:58:48 +0900

    Initial test code

--- src/krxreader/base.py
+++ src/krxreader/base.py
@@ -1,11 +1,12 @@
 import csv
+import logging
 
 from .fetch import get_json_data
 from .fetch import download_csv
 
 
 class KrxBase:
-    def __init__(self, date, start, end):
+    def __init__(self, date=None, start=None, end=None):
         self._date = date
         self._start = start
         self._end = end
@@ -22,11 +23,12 @@ class KrxBase:
         payload.update({
             'csvxls_isNo': self._csvxls_is_no
         })
-        print(payload)
 
-        dic = get_json_data(payload)
+        logging.debug(payload)
 
-        return dic
+        data = get_json_data(payload)
+
+        return data
 
     def fetch_data(self, bld, params):
         payload = {
@@ -38,7 +40,8 @@ class KrxBase:
             'name': 'fileDown',
             'url': bld
         })
-        print(payload)
+
+        logging.debug(payload)
 
         csv_str = download_csv(payload)
 

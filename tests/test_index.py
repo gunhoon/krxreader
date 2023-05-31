50c14533edcfb99966216038ef5b942a8b839c32
Gunhoon Lee <gunhoon@gmail.com>
2023-05-31 23:20:49 +0900

    Complete the implementation of bond index

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,4 +1,5 @@
 from krxreader.index import StockIndex
+from krxreader.index import BondIndex
 
 
 def test_index_price():
@@ -15,3 +16,26 @@ def test_index_price_change():
 
     assert data[1][0] == 'KRX 300'
     assert data[1][2] == '1533.13'
+
+
+class TestBondIndex:
+    def test_maintenance(self):
+        index = BondIndex()
+        data = index.index_price()
+
+        assert len(data) == 4
+        assert len(data[0]) == 14
+
+    def test_index_price(self):
+        index = BondIndex('20230526')
+        data = index.index_price()
+
+        assert data[1][0] == 'KRX 채권지수'
+        assert data[1][1] == '180.37'
+
+    def test_price_by_index(self):
+        index = BondIndex(start='20230519', end='20230526')
+        data = index.price_by_index('국고채프라임지수')
+
+        assert data[1][0] == '2023/05/26'
+        assert data[1][1] == '181.05'

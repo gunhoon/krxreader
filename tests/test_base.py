da726fefe13cd8965e827e8621254724d1c983bf
Gunhoon Lee <gunhoon@gmail.com>
2024-01-19 10:54:26 +0900

    Refactoring base module

--- tests/test_base.py
+++ tests/test_base.py
@@ -17,12 +17,12 @@ def test__trading_date():
     assert _trading_date(dt1) == '20230519'
     assert _trading_date(dt2) == '20230519'
     assert _trading_date(dt3) == '20230519'
-    assert _trading_date(dt1, base_time=9) == '20230519'
-    assert _trading_date(dt2, base_time=9) == '20230519'
-    assert _trading_date(dt3, base_time=9) == '20230519'
-    assert _trading_date(dt1, base_time=24) == '20230519'
-    assert _trading_date(dt2, base_time=24) == '20230519'
-    assert _trading_date(dt3, base_time=24) == '20230519'
+    assert _trading_date(dt1, base_hour=9) == '20230519'
+    assert _trading_date(dt2, base_hour=9) == '20230519'
+    assert _trading_date(dt3, base_hour=9) == '20230519'
+    assert _trading_date(dt1, base_hour=24) == '20230519'
+    assert _trading_date(dt2, base_hour=24) == '20230519'
+    assert _trading_date(dt3, base_hour=24) == '20230519'
 
     # 일요일
     dt4 = datetime.datetime.fromisoformat('2023-05-21 08:59:59.501235+09:00')
@@ -32,12 +32,12 @@ def test__trading_date():
     assert _trading_date(dt4) == '20230519'
     assert _trading_date(dt5) == '20230519'
     assert _trading_date(dt6) == '20230519'
-    assert _trading_date(dt4, base_time=9) == '20230519'
-    assert _trading_date(dt5, base_time=9) == '20230519'
-    assert _trading_date(dt6, base_time=9) == '20230519'
-    assert _trading_date(dt4, base_time=24) == '20230519'
-    assert _trading_date(dt5, base_time=24) == '20230519'
-    assert _trading_date(dt6, base_time=24) == '20230519'
+    assert _trading_date(dt4, base_hour=9) == '20230519'
+    assert _trading_date(dt5, base_hour=9) == '20230519'
+    assert _trading_date(dt6, base_hour=9) == '20230519'
+    assert _trading_date(dt4, base_hour=24) == '20230519'
+    assert _trading_date(dt5, base_hour=24) == '20230519'
+    assert _trading_date(dt6, base_hour=24) == '20230519'
 
     # 월요일
     dt7 = datetime.datetime.fromisoformat('2023-05-22 08:59:59.501235+09:00')
@@ -47,12 +47,12 @@ def test__trading_date():
     assert _trading_date(dt7) == '20230522'
     assert _trading_date(dt8) == '20230522'
     assert _trading_date(dt9) == '20230522'
-    assert _trading_date(dt7, base_time=9) == '20230519'
-    assert _trading_date(dt8, base_time=9) == '20230522'
-    assert _trading_date(dt9, base_time=9) == '20230522'
-    assert _trading_date(dt7, base_time=24) == '20230519'
-    assert _trading_date(dt8, base_time=24) == '20230519'
-    assert _trading_date(dt9, base_time=24) == '20230519'
+    assert _trading_date(dt7, base_hour=9) == '20230519'
+    assert _trading_date(dt8, base_hour=9) == '20230522'
+    assert _trading_date(dt9, base_hour=9) == '20230522'
+    assert _trading_date(dt7, base_hour=24) == '20230519'
+    assert _trading_date(dt8, base_hour=24) == '20230519'
+    assert _trading_date(dt9, base_hour=24) == '20230519'
 
 
 def test_date():
@@ -115,7 +115,7 @@ def params():
     }
 
 
-@pytest.mark.skipif(True, reason='requires http request')
+@pytest.mark.skipif(False, reason='requires http request')
 def test_fetch_json(bld, params):
     base = KrxBase()
 
@@ -125,7 +125,7 @@ def test_fetch_json(bld, params):
     assert data[1][5] == '2,875'
 
 
-@pytest.mark.skipif(True, reason='requires http request')
+@pytest.mark.skipif(False, reason='requires http request')
 def test_fetch_csv(bld, params):
     base = KrxBase()
 
